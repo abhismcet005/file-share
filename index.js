40969860fa9d8df35d8faae1deb5eac7f9216cc9
Abhishek Singh <abhishek.singh@tothenew.com>
2015-11-18 12:33:27 +0530

    added validations for invalid port and directory

--- index.js
+++ index.js
@@ -2,21 +2,37 @@
 
 // Set default node environment to development
 var express = require('express');
-var app = express();
 var directory = require('serve-index');
-
-//var userArgs = process.argv.slice(2);
-//var userPort = userArgs[0] || 9000;
-//var dirUrl = userArgs[1] || __dirname;
-//console.log(userPort,dirUrl);
-
+var fs = require('fs');
 var userArgs = require('optimist').argv;
-var userPort = userArgs.port || 9000;
-var dirUrl = userArgs.path || __dirname;
 
-app.use(directory(dirUrl));
-app.use(express.static(dirUrl));
+var app = express();
+var userPort = userArgs.port;
+var dirUrl = userArgs.path;
 
-var server = app.listen(userPort, function () {
-    console.log('file-share module listening at port : '+ userPort);
-});
+if(!userPort || userPort < 1024 || userPort > 49151){
+    userPort = 9000;
+    console.log("Invalid port entered, will use default port 9000.");
+}
+
+if(dirUrl){
+    fs.stat(dirUrl,function(err,stat){
+        if(err || !stat.isDirectory()){
+            console.log("Invalid directory input, will serve current directory.");
+            dirUrl = __dirname;
+        }
+
+        serveDirectory();
+    });
+}else{
+    dirUrl = __dirname;
+    serveDirectory();
+}
+
+function serveDirectory(){
+    app.listen(userPort, function () {
+        console.log('file-share module listening at port : '+ userPort);
+        app.use(directory(dirUrl));
+        app.use(express.static(dirUrl));
+    });
+}
